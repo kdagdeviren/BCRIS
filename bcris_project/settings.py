8efa18cf4dfd684b491f6ab4f960e5748629202c
Okan Kantar <okkant@gmail.com>
2025-12-05 16:03:08 +0300

    set1

--- bcris_project/settings.py
+++ bcris_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fertix.xyz', 'www.fertix.xyz', 'bcris.site', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['fertix.xyz', 'www.fertix.xyz', 'bcris.site', 'www.bcris.site' 'localhost', '127.0.0.1']
 
 
 # Application definition
